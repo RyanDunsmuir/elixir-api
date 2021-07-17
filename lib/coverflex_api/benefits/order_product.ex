7c6a86eea349cd976d246ecc5f9dc449820feaeb
Ryan Dusmuir <ryan_dunsmuir@kansamples.com>
2021-07-17 10:36:54 +0100

    removed unnecessary code

--- lib/coverflex_api/benefits/order_product.ex
+++ lib/coverflex_api/benefits/order_product.ex
@@ -1,6 +1,10 @@
 defmodule CoverflexApi.Benefits.OrderProduct do
   use Ecto.Schema
   import Ecto.Changeset
+  import Ecto.Query, warn: false
+
+  alias CoverflexApi.Benefits.OrderProduct
+  alias CoverflexApi.Repo
 
   schema "orderproducts" do
     belongs_to :order, CoverflexApi.Benefits.Order
@@ -14,5 +18,21 @@ defmodule CoverflexApi.Benefits.OrderProduct do
     order_product
     |> cast(attrs, [:order_id, :product_id])
     |> validate_required([:order_id, :product_id])
+  end
+
+  def with_orders(order_product_id) do
+    Repo.get(OrderProduct, order_product_id)
+    |> Repo.preload(:orders)
+  end
+
+  def with_orders(order_product_id) do
+    Repo.get(OrderProduct, order_product_id)
+    |> Repo.preload(:products)
+  end
+
+  def product(order_product_id) do
+    with_orders(order_product_id)
+    order_product = Repo.get(OrderProduct, order_product_id)
+    order_product.product
   end
 end
--- lib/coverflex_api/benefits/user.ex
+++ lib/coverflex_api/benefits/user.ex
@@ -1,12 +1,16 @@
 defmodule CoverflexApi.Benefits.User do
   use Ecto.Schema
   import Ecto.Changeset
+  import Ecto.Query, warn: false
+
+  alias CoverflexApi.Benefits
+  alias CoverflexApi.Repo
 
   schema "users" do
     field :balance, :float
     field :username, :string
 
-    has_many :orders, CoverflexApi.Benefits.Order
+    has_many :orders, Benefits.Order
 
     has_many :orderproducts,
       through: [:orders, :orderproducts]
@@ -20,4 +24,25 @@ defmodule CoverflexApi.Benefits.User do
     |> cast(attrs, [:username, :balance])
     |> validate_required([:username, :balance])
   end
+
+  def with_orders(user_id) do
+    Benefits.get_user!(user_id)
+    |> Repo.preload(:order)
+  end
+
+  def with_order_products(user_id) do
+    Benefits.get_user!(user_id)
+    |> Repo.preload(:orderproducts)
+  end
+
+  # def products do
+  #   # find the user using id
+  #   user = Benefits.get_user!(user_id)
+  #   # preload users orderproducts
+  #   with_order_products(user.id)
+  #   # store orderproducts in a variable
+  #   orderproducts = user.orderproducts
+  #   # iterate through the users orderproducts and return their associated products
+  #   Enum.map(orderproducts, fn orderproduct -> orderproduct.product(orderproduct.id) end)
+  # end
 end
