99afbd9a4f13b75f2dacecfa2d85426b8df3554d
Ryan Dusmuir <ryan_dunsmuir@kansamples.com>
2021-07-18 20:38:00 +0100

    finished writing process for create order and tested (it should work), but now getting 400 error when testing POST request on postman

--- lib/coverflex_api_web/controllers/order_controller.ex
+++ lib/coverflex_api_web/controllers/order_controller.ex
@@ -18,20 +18,28 @@ defmodule CoverflexApiWeb.OrderController do
     balance = user.balance
 
     # find the products in the database and throw an error if they dont exist
-    products = Enum.map(items, fn item -> validate_product(item, order_total) end)
+    products = Enum.map(items, fn item -> validate_product(item, order_total, conn) end)
 
     # get the product names and create a list to compare with user product_ids
     product_names = Enum.map(products, fn product -> product.name end)
 
     # check if the user already has the products and throw an error if they have
-    Enum.each(user.product_ids, fn product_id -> Enum.member?(product_names, product_id) end)
+    Enum.each(user.product_ids, fn product_id ->
+      if !Enum.member?(product_names, product_id) do
+        render(conn, "products_already_purchased.json")
+      end
+    end)
 
     # calculate the order total and check if the user has sufficient balance
     # subtract the total from the users balance
     if balance < order_total do
+      render(conn, "insufficient_balance.json")
     else
       balance = balance - order_total
     end
+
+    # add the product names to users product_ids
+    user.product_ids ++ product_names
 
     # create the order and display it
     order = Benefits.create_order(user_id: user.id, total: order_total)
@@ -43,15 +51,13 @@ defmodule CoverflexApiWeb.OrderController do
     #   |> put_resp_header("location", Routes.order_path(conn, :show, order))
     #   |> render("show.json", %{order: order, product_ids: product_names})
     # end
-    # add the product names to users product ids
-    user.product_ids ++ product_names
   end
 
-  def validate_product(name, order_total) do
+  def validate_product(name, order_total, conn) do
     product = Benefits.get_product!(name)
 
     if product == nil do
-      # throw error "product doesnt exist"
+      render(conn, "products_not_found.json")
     else
       order_total = order_total + product.price
       product
